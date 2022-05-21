f1b5cc563b7b809ce984aba928426fc09600433e
NGS <kusun1020@gmail.com>
2022-05-21 18:04:51 +0900

    Java_Practice
    완성 파일 작성

---
+++ Java_Practice_1/src/Package0326/Package_Buyer/Computer.java
@@ -0,0 +1,19 @@
+package Package_Buyer;
+
+public class Computer extends Product {
+
+	// 생성자
+	//  빈 생성자
+	Computer() {
+		
+	}
+
+	//  가격을 초기화하는 생성자
+	Computer(int price) {
+		super(price);
+	}
+	
+	public String toString() {
+		return "Computer";
+	}
+}
